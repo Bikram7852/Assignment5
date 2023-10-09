1177d519d773bef802cd03b07a8ae511be87f854
Bikram7852 <bikram.roy7852@gmail.com>
2023-10-09 17:20:59 +0530

    Add project files.

---
+++ Assignment5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankingBusinessLogicLayer;
+
+namespace Assignment5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank bank = new Bank();
+            bank.AccountNo = 37570696888;
+            bank.BranchName = "Kolkata";
+            bank.DepositAmount = 10000;
+            bank.DepositDate = new DateTime(2022,05,10);
+            bank.WithdrawAmount = 1000;
+            bank.WithdrawDate = new DateTime(2023,06,12);
+            bank.Deposit(bank.DepositAmount, bank.DepositDate, bank.BranchName, bank.AccountNo);
+            bank.Withdraw(bank.WithdrawAmount,bank.WithdrawDate,bank.BranchName, bank.AccountNo);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BankingBusinessLogicLayer/Bank.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingBusinessLogicLayer
+{
+    public class Bank
+    {
+        public float DepositAmount { get; set; }
+        public DateTime DepositDate { get; set; }
+        public string BranchName { get; set; }
+        public long AccountNo { get; set; }
+        public DateTime WithdrawDate { get; set; }
+        public float WithdrawAmount { get; set; }
+        public void Deposit(float amt, DateTime depositDate, string branchName, long accNo)
+        {
+            DepositAmount = amt;
+            DepositDate = depositDate;
+            BranchName = branchName;
+            AccountNo = accNo;
+            Console.WriteLine("Account No: " + AccountNo+ " Branch Name: " + BranchName+" Deposit Amount: " + DepositAmount + " Deposit Date: "+DepositDate);
+        }
+        public void Deposit(long accNo, float amt, DateTime depositDate, string branchName)
+        {
+            DepositAmount = amt;
+            DepositDate = depositDate;
+            BranchName = branchName;
+            AccountNo = accNo;
+            Console.WriteLine("Account No: " + AccountNo + " Branch Name: " + BranchName + " Deposit Amount: " + DepositAmount + " Deposit Date: " + DepositDate);
+        }
+        public void Deposit(long accNo, float amt, DateTime depositDate)
+        {
+            DepositAmount = amt;
+            DepositDate = depositDate;
+            AccountNo = accNo;
+            Console.WriteLine("Account No: " + AccountNo + " Branch Name: " + BranchName + " Deposit Amount: " + DepositAmount + " Deposit Date: " + DepositDate);
+        }
+        public void Withdraw(float amt, DateTime withdrawDate, string branchName, long accNo)
+        {
+            WithdrawAmount = amt;
+            WithdrawDate = withdrawDate;
+            BranchName = branchName;
+            AccountNo = accNo;
+            Console.WriteLine("Account No: " + AccountNo + " Branch Name: " + BranchName + " Withdraw Amount: " + WithdrawAmount + " Withdraw Date: " + WithdrawDate);
+        }
+    }
+}
